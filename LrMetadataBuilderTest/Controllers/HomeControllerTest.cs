88c394d6eaadf566f5d209d177acd22c6ab28ea4
mikegatiss <mike.gatiss@gmail.com>
2018-07-26 22:08:50 +0100

    Having problems with Test setup so have moved code back into test method. Added mock repository setup.

--- LrMetadataBuilderTest/Controllers/HomeControllerTest.cs
+++ LrMetadataBuilderTest/Controllers/HomeControllerTest.cs
@@ -13,25 +13,64 @@ namespace lrMetadataBuilderTest.Controllers
 {
     public class HomeControllerTest
     {
-        private readonly Mock<IEventRepository> _mockRepository;
-        private readonly HomeController _sut;
-
-
-        public HomeControllerTest()
-        {
-            _mockRepository = new Mock<IEventRepository>();
-            _sut = new HomeController(_mockRepository.Object);
-
-        }
-
         public class Index
         {
             [Fact]
             public void ReturnsViewResult()
             {
-                IActionResult result = _sut.Index();
+                var mockRepository= new Mock<IEventRepository>();
+                var sut = new HomeController(mockRepository.Object);
+            
+                var result = sut.Index();
 
                 Assert.IsType<ViewResult>(result);
+
+            }
+        }
+        public class Details
+        {
+
+            [Fact]
+            public void ReturnsNotFoundResult()
+            {
+                //passing an id to Details with no corresponding event 
+                // should return not found
+                var mockRepository= new Mock<IEventRepository>();
+                var sut = new HomeController(mockRepository.Object);
+                var result = sut.Details(1);
+
+                Assert.IsType<NotFoundResult>(result);
+            }
+
+            [Fact]
+            public void ReturnsViewResult()
+            {
+                //this time set up populated repository
+                IList<Event> events = new List<Event>
+                {
+                    new Event {Id=1,Name="Event Name 1", EventDate="20180714",Description="MRDA Tier 1 North Playoffs",Cancelled = false,VenueId = 1},
+                    new Event {Id=2,Name="Event Name 2", EventDate="20180714",Description="Friendly match",Cancelled = false,VenueId = 1},
+                    new Event {Id=3,Name="Event Name 3", EventDate="20180714",Description="Friendly match",Cancelled = false,VenueId = 2},
+                    new Event {Id=4,Name="Event Name 4", EventDate="20180714",Description="MRDA Tier 3 South Playoffs",Cancelled = false,VenueId = 1},
+                    new Event {Id=5,Name="Event Name 5", EventDate="20180714",Description="MRDA Tier 2 North Playoffs",Cancelled = false,VenueId = 2}
+
+                };
+
+                //Mock the event repository
+                Mock<IEventRepository> mockEventRepository = new Mock<IEventRepository>();
+                //Return all events
+                mockEventRepository.Setup(mr => mr.GetAllEvents()).Returns(events);
+                mockEventRepository.Setup(mr => mr.GetEventById(
+                    It.IsAny<int>())).Returns((int i) => events.Where(
+                    x => x.Id == i).Single());
+
+                var MockEventRepository = mockEventRepository.Object;
+
+                Event testEvent = MockEventRepository.GetEventById(1);
+                Assert.NotNull(testEvent);
+                Assert.IsType<Event>(testEvent);
+                Assert.Equal("Event Name 1",testEvent.Name);
+
 
             }
         }

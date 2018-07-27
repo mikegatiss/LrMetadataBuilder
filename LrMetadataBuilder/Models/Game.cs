51a24046d20a1c25b23433f689cd97b4569c2d1e
mikegatiss <mike.gatiss@gmail.com>
2018-07-27 11:48:07 +0100

    added League, Team and Game classes

---
+++ LrMetadataBuilder/Models/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages.Internal;
+
+namespace LrMetadataBuilder.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? EventId { get; set; } // needs to be nullable as not all games are part of bigger event 
+        [ForeignKey("HomeTeam")]
+        public int HomeTeamId { get; set; }
+        [ForeignKey("AwayTeam")]
+        public int AwayTeamId { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+
+        public virtual Team HomeTeam { get; set; }
+        public virtual Team AwayTeam { get; set; }
+    }
+}
---
+++ LrMetadataBuilder/Models/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LrMetadataBuilder.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [ForeignKey("League")]
+        public int LeagueId { get; set; }
+
+        public virtual League League { get; set; }
+    }
+}
